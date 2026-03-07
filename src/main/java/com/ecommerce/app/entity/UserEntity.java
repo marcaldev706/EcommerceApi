4a3e09defde1fc979c9e511cd4d6fe6e79df78b9
gustavomarcal <gustavomarcaldev@gmail.com>
2026-03-07 12:41:13 -0300

    feat(user): create repository

--- src/main/java/com/ecommerce/app/entity/UserEntity.java
+++ src/main/java/com/ecommerce/app/entity/UserEntity.java
@@ -1,17 +1,29 @@
 package com.ecommerce.app.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class UserEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name", nullable = false, length = 80)
     private String name;
+    @Column(name = "email", nullable = false, length = 80, unique = true)
     private String email;
+    @Column(name = "cpf", nullable = false, length = 11, unique = true)
     private String cpf;
+    @Column(name = "rg", nullable = false, length = 11, unique = true)
     private String rg;
+    @Column(name = "date_creation", nullable = false)
     private LocalDateTime createdAt;
 }
