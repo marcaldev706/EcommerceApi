c30afc90b042542a0191e4569247ac8d7cc864cd
gustavomarcal <gustavomarcaldev@gmail.com>
2026-03-08 18:05:23 -0300

    feat(product): create  entity

---
+++ src/main/java/com/ecommerce/app/product/ProductEntity.java
@@ -0,0 +1,16 @@
+package com.ecommerce.app.product;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductEntity {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+}
